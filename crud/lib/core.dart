191002d51baa2977a3444f33c11de82b083b3c46
khibbanfais10 <khibbanfais10@gmail.com>
2023-08-01 00:07:26 +0700

    update
    update

--- crud/lib/core.dart
+++ crud/lib/core.dart
@@ -5,11 +5,8 @@ it means you have to rename it to something more unique.
 */
 export 'package:crud/module/add/controller/add_controller.dart';
 export 'package:crud/module/add/view/add_view.dart';
-export 'package:crud/module/edit/controller/edit_controller.dart';
-export 'package:crud/module/edit/view/edit_view.dart';
 
-export 'package:crud/module/list_user/controller/list_user_controller.dart';
-export 'package:crud/module/list_user/view/list_user_view.dart';
+
 export 'package:crud/module/mainmenu/controller/mainmenu_controller.dart';
 export 'package:crud/module/mainmenu/view/mainmenu_view.dart';
 
